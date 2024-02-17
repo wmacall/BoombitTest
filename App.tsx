afde31667f05387820d2e6009222a9891d6f28ce
wmacall <macallwalter@gmail.com>
2024-02-16 18:04:35 -0600

    🎉 Initialize project, add folder structure

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <SafeAreaView />
+      <Text>Boombit Test</Text>
+    </View>
+  );
+};
+
+export default App;
